3cb8fac38b5989f59caa76bed967ee7984c11c7b
Lefevre Anthony <lefevre.anthony.dev@gmail.com>
2019-06-05 22:35:51 +0200

    rectification land

--- Eagle.java
+++ Eagle.java
@@ -24,7 +24,7 @@ public class Eagle extends Bird implements Fly {
     }
     @Override
     public void takeOff() {
-        if (!this.flying && this.altitude == 0) {
+        if (!this.flying) {
             this.flying = true;
             System.out.printf("%s takes off in the sky.%n", this.getName());
         }
@@ -53,7 +53,7 @@ public class Eagle extends Bird implements Fly {
     
     @Override
     public void land() {
-        if (this.flying && this.altitude == 1) {
+        if (this.flying && this.altitude == 0) {
             System.out.printf("%s lands on the ground.%n", this.getName());
         } else {
             System.out.printf("%s is too high, it can't lands.%n", this.getName());}
