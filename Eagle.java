cda30a9efb35381f12d4d2e6567ec8e09e3be0e7
Lefevre Anthony <lefevre.anthony.dev@gmail.com>
2019-06-06 09:17:08 +0200

    land modif

--- Eagle.java
+++ Eagle.java
@@ -53,7 +53,7 @@ public class Eagle extends Bird implements Fly {
     
     @Override
     public void land() {
-        if (!this.flying && this.altitude == 0) {
+        if (!this.flying && this.altitude == 1) {
             System.out.printf("%s lands on the ground.%n", this.getName());
         } else {
             System.out.printf("%s is too high, it can't lands.%n", this.getName());}
